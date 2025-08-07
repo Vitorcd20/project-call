0393aba698893e6c47ca5b395ee5bc6d4dd2f6dd
Vitor Vieira <vitor.vieira@omd.com.br>
2025-08-07 14:43:53 -0300

    feat: react-hook-form

--- meu-projeto/pages/home/components/ClaimUsernameForm/index.tsx
+++ meu-projeto/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,35 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
-import { Form } from './styles'
+import { Button, TextInput } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+import { Form } from "./styles";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>;
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  async function handleClaimUsername(data: any) {
+    console.log(data);
+  }
+
   return (
-    <Form as="form">
-      <TextInput  size="sm" prefix="projectapp.com/" placeholder="seu-usuário"  />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="projectapp.com/"
+        placeholder="seu-usuário"
+        {...register("username")}
+      />
+
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
       </Button>
     </Form>
-  )
+  );
 }
